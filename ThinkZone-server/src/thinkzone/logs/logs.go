9de0f6ac42dc537bcff375a114c040ca52104a9e
portaloffreedom <matteo.dek@gmail.com>
2012-07-15 12:20:57 +0200

    File di log gestiti per bene, vengono chiusi alla chiusura (solo col segnale interrupt -> ctrl-c) creato uno script per pulire eseguibile e tutti i file di log

--- ThinkZone-server/src/thinkzone/logs/logs.go
+++ ThinkZone-server/src/thinkzone/logs/logs.go
@@ -4,19 +4,34 @@ package logs
 import (
 	"fmt"
 	"os"
-	//	"os/signal"
+	"os/signal"
+	"strconv"
 	"strings"
-	//	"strconv"
+	"time"
 )
 
 var (
 	stampa_su_terminale bool   = false
-	logFilename         string = "thinkzone-server.log"
+	logFilename         string = "thinkzone-server"
 	logFile             *os.File
+	lastDate            string = ""
 )
+
+func getTimeString(t time.Time) (date, clock string) {
+	hour, min, sec := t.Clock()
+	year, month, day := t.Date()
+	clock = strings.Join([]string{strconv.Itoa(hour), strconv.Itoa(min), strconv.Itoa(sec)}, ".")
+	date = strings.Join([]string{strconv.Itoa(day), month.String(), strconv.Itoa(year)}, "-")
+	return
+}
 
 func init() {
 	fmt.Println("Inizializzo il file di log")
+
+	now := time.Now()
+	date, clock := getTimeString(now)
+	logFilename = strings.Join([]string{logFilename, date, clock}, "_") + ".log"
+
 	var err error
 	logFile, err = os.Create(logFilename)
 	if err != nil {
@@ -26,16 +41,26 @@ func init() {
 	logFile.WriteString("File di log del server di ThinkZone\n")
 
 	c := make(chan os.Signal, 1)
-	//	signal.Notify(c)	
+	signal.Notify(c, os.Interrupt)
 
 	go func(c chan os.Signal) {
-		s := <-c
-		fmt.Println("catturato un segnale")
+		sig := <-c
 		//		if s == os.Kill {
-		fmt.Println("segnale catturato:", s)
+		fmt.Println("\nsegnale catturato:", sig)
 		fmt.Println("chiusura del file di log in corso")
-		logFile.Close()
+		if logFile != nil {
+			logFile.Close()
+		} else {
+			fmt.Println("impossibile chiudere il file di log")
+		}
 		//		}
+		myself, err := os.FindProcess(os.Getpid())
+		if err != nil {
+			fmt.Println("ma che cazz? non riesco a trovare me stesso?\n\tmotivo:", err)
+			return
+		}
+		myself.Signal(os.Kill)
+
 		return
 	}(c)
 }
@@ -62,6 +87,14 @@ func Log(messageArray ...string) {
 }
 
 func stampaSuFile(message string) {
-	logFile.WriteString(message)
+
+	date, clock := getTimeString(time.Now())
+
+	if lastDate != date {
+		logFile.WriteString("\n" + date + "\n")
+		lastDate = date
+	}
+
+	logFile.WriteString(clock + ": " + message)
 	logFile.WriteString("\n")
 }
