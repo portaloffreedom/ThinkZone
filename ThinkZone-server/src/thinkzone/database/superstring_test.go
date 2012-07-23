e7b864d7c57ff836a93860aa6c36b7b6ebd3f07b
portaloffreedom <matteo.dek@gmail.com>
2012-07-23 15:01:11 +0200

    corretto testing

--- ThinkZone-server/src/thinkzone/database/superstring_test.go
+++ ThinkZone-server/src/thinkzone/database/superstring_test.go
@@ -50,49 +50,49 @@ func TestSuperString(test *testing.T) {
 	testEqual("", "[0]")
 
 	//#2 ---insert---
-	prova.insStringElem("99", 0)
+	prova.InsStringElem("99", 0)
 	testEqual("99", "99[2]")
 
 	//#3
-	prova.insStringElem("12!", 0)
+	prova.InsStringElem("12!", 0)
 	testEqual("12!99", "12![3]99[2]")
 
 	//#4
-	prova.insStringElem("###", 3)
+	prova.InsStringElem("###", 3)
 	testEqual("12!###99", "12!###[6]99[2]")
 
 	//#5
-	prova.insStringElem("tro", 2)
+	prova.InsStringElem("tro", 2)
 	testEqual("12tro!###99", "12tro[5]!###[4]99[2]")
 
 	//#6 ----delete----
-	prova.delElem(1, 1)
+	prova.DelElem(1, 1)
 	testEqual("1tro!###99", "1tro[4]!###[4]99[2]")
 
 	//#7
-	prova.delElem(4, 1)
+	prova.DelElem(4, 1)
 	testEqual("1tro###99", "1tro[4]###[3]99[2]")
 
 	//#8
-	prova.delElem(4, 3)
+	prova.DelElem(4, 3)
 	testEqual("1tro99", "1tro[4]99[2]")
 
 	//#9
-	prova.delElem(3, 1)
+	prova.DelElem(3, 1)
 	testEqual("1tr99", "1tr[3]99[2]")
 
 	//#10
-	prova.delElem(0, 3)
+	prova.DelElem(0, 3)
 	testEqual("99", "99[2]")
 
 	//#11
-	prova.insStringElem("porcoDioZoccolo", 0)
-	prova.insStringElem(" ", 8)
-	prova.insStringElem(" ", 5)
+	prova.InsStringElem("porcoDioZoccolo", 0)
+	prova.InsStringElem(" ", 8)
+	prova.InsStringElem(" ", 5)
 	testEqual("porco Dio Zoccolo99", "porco [6]Dio [4]Zoccolo[7]99[2]")
 
 	//#12
-	prova.delElem(3, 15)
+	prova.DelElem(3, 15)
 	testEqual("por9", "por[3]9[1]")
 
 	fmt.Println()
@@ -106,11 +106,11 @@ func TestSuperString2(test *testing.T) {
 	testEqual("", "[0]")
 
 	//#2
-	prova.insStringElem("Australopitecus---", 0)
+	prova.InsStringElem("Australopitecus---", 0)
 	testEqual("Australopitecus---", "Australopitecus---[18]")
 
 	//#3
-	prova.insStringElem("0", 16)
+	prova.InsStringElem("0", 16)
 	testEqual("Australopitecus-0--", "Australopitecus-0[17]--[2]")
 
 }
